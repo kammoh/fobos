97a30957d176f13e1ebaf67cec293cdebf2bf677
rajesh velegalati <rvelegal@newdpa.(none)>
2013-08-25 22:31:41 -0400

    new stuff

--- test/Prog_OpenADCTest.sh
+++ test/Prog_OpenADCTest.sh
@@ -1,2 +1,2 @@
-echo "Programming Nexys3 Board with OpenADC test Firmware"
-djtgcfg prog -i 0 -d Nexys3 -f /local/rvelegal/FPGA/Nexys2OpenADC/Nexys2_ctrl.bit
+echo "Programming Nexys2 Board with OpenADC test Firmware"
+djtgcfg prog -i 0 -d Nexys2 -f /local/rvelegal/FPGA/Nexys2OpenADC/Nexys2_ctrl.bit
