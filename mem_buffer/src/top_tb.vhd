bc74ce33904ac55662a59f9d5f3a11febaa661b3
Jeremy Barthelemy <j.l.barthel@gmail.com>
2013-09-05 17:06:56 -0400

    some mods

--- mem_buffer/src/top_tb.vhd
+++ mem_buffer/src/top_tb.vhd
@@ -1,61 +1,44 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use IEEE.std_logic_unsigned.all;
  
-
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
 ENTITY top_tb IS
 END top_tb;
  
 ARCHITECTURE behavior OF top_tb IS 
+ 
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT TOP
+    COMPONENT top
     PORT(
          Input : IN  std_logic_vector(15 downto 0);
-         CLOCK : IN  std_logic;
+         Output : OUT  std_logic_vector(15 downto 0);
+         process_data : IN  std_logic;
          RESET : IN  std_logic;
-         MUX_SEL : IN  std_logic;
-         DRAM_ADDR : IN  std_logic_vector(10 downto 0);
-         FIFO1_RD_EN : IN  std_logic;
-         FIFO2_RD_EN : IN  std_logic;
-         FIFO1_WR_EN : IN  std_logic;
-         FIFO2_WR_EN : IN  std_logic;
-         DRAM_WR_EN : IN  std_logic;
-			--REG_EN : IN STD_LOGIC;
-         FIFO1_FULL : OUT  std_logic;
-         FIFO2_FULL : OUT  std_logic;
-         FIFO1_EMPTY : OUT  std_logic;
-         FIFO2_EMPTY : OUT  std_logic;
-			fif1out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); --test signal!
-			fif2out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); --test signal!
-			muxout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); --test signal!
-         Output : OUT  std_logic_vector(15 downto 0)
+         CLOCK : IN  std_logic;
+         Stall : OUT  std_logic;
+         fif1out : OUT  std_logic_vector(15 downto 0);
+         fif2out : OUT  std_logic_vector(15 downto 0);
+         muxout : OUT  std_logic_vector(15 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
    signal Input : std_logic_vector(15 downto 0) := (others => '0');
-   signal CLOCK : std_logic := '0';
+   signal process_data : std_logic := '0';
    signal RESET : std_logic := '0';
-   signal MUX_SEL : std_logic := '0';
-   signal DRAM_ADDR : std_logic_vector(10 downto 0) := (others => '0');
-   signal FIFO1_RD_EN : std_logic := '0';
-   signal FIFO2_RD_EN : std_logic := '0';
-   signal FIFO1_WR_EN : std_logic := '0';
-   signal FIFO2_WR_EN : std_logic := '0';
-   signal DRAM_WR_EN : std_logic := '0';
-	--signal REG_EN : STD_LOGIC := '0';
+   signal CLOCK : std_logic := '0';
 
  	--Outputs
-   signal FIFO1_FULL : std_logic;
-   signal FIFO2_FULL : std_logic;
-   signal FIFO1_EMPTY : std_logic;
-   signal FIFO2_EMPTY : std_logic;
-	signal fif1out : std_logic_vector(15 downto 0);
-	signal fif2out : std_logic_vector(15 downto 0);
-	signal muxout : std_logic_vector(15 downto 0); 
    signal Output : std_logic_vector(15 downto 0);
+   signal Stall : std_logic;
+   signal fif1out : std_logic_vector(15 downto 0);
+   signal fif2out : std_logic_vector(15 downto 0);
+   signal muxout : std_logic_vector(15 downto 0);
 
    -- Clock period definitions
    constant CLOCK_period : time := 10 ns;
@@ -63,26 +46,16 @@ ARCHITECTURE behavior OF top_tb IS
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: TOP PORT MAP (
+   uut: top PORT MAP (
           Input => Input,
-          CLOCK => CLOCK,
+          Output => Output,
+          process_data => process_data,
           RESET => RESET,
-          MUX_SEL => MUX_SEL,
-          DRAM_ADDR => DRAM_ADDR,
-          FIFO1_RD_EN => FIFO1_RD_EN,
-          FIFO2_RD_EN => FIFO2_RD_EN,
-          FIFO1_WR_EN => FIFO1_WR_EN,
-          FIFO2_WR_EN => FIFO2_WR_EN,
-          DRAM_WR_EN => DRAM_WR_EN,
-			 --REG_EN => REG_EN,
-          FIFO1_FULL => FIFO1_FULL,
-          FIFO2_FULL => FIFO2_FULL,
-          FIFO1_EMPTY => FIFO1_EMPTY,
-          FIFO2_EMPTY => FIFO2_EMPTY,
-			 fif1out => fif1out,
-			 fif2out => fif2out,
-			 muxout => muxout,
-          Output => Output
+          CLOCK => CLOCK,
+          Stall => Stall,
+          fif1out => fif1out,
+          fif2out => fif2out,
+          muxout => muxout
         );
 
    -- Clock process definitions
@@ -100,98 +73,9 @@ BEGIN
    begin		
       -- hold reset state for 100 ns.
       wait for 100 ns;	
-		
+
       wait for CLOCK_period*10;
-		
-		--Temp
-		--Reading into FIFOS
-		FIFO1_WR_EN <= '1';
-		FIFO2_WR_EN <= '0';
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '0';
-		Input <= "0000000000000001";
-		MUX_SEL <= '0';
-		DRAM_WR_EN <= '0';
-		DRAM_ADDR <= "00000000000";
-		wait for clock_period;
 
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '1';
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '0';
-		Input <= "0000000000000010";
-		DRAM_WR_EN <= '0';
-		MUX_SEL <= '0';
-		DRAM_ADDR <= "00000000000";
-		wait for clock_period;
-
-		FIFO1_WR_EN <= '1';
-		FIFO2_WR_EN <= '0';
-		Input <= "0000000000000100";
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '0';
-		DRAM_WR_EN <= '0';
-		MUX_SEL <= '0';
-		DRAM_ADDR <= "00000000000";
-		wait for clock_period;
-		
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '1';
-		Input <= "0000000000001000";
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '0';
-		DRAM_WR_EN <= '0';
-		MUX_SEL <= '0';
-		DRAM_ADDR <= "00000000000";
-		wait for clock_period;
-		
-		--Reading into DRAM
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '0';
-		Input <= "1111111111111111";
-		FIFO1_RD_EN <= '1';
-		FIFO2_RD_EN <= '0';
-		DRAM_WR_EN <= '1';
-		DRAM_ADDR <= "00000000000";
-		wait for clock_period;
-		
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '0';
-		Input <= "1111111111111111";
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '1';
-		--REG_EN <= '1';
-		DRAM_WR_EN <= '1';
-		MUX_SEL <= '0';
-		DRAM_ADDR <= "00000000001";
-		wait for clock_period;
-		
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '0';
-		Input <= "1111111111111111";
-		FIFO1_RD_EN <= '1';
-		FIFO2_RD_EN <= '0';
-		DRAM_WR_EN <= '1';
-		MUX_SEL <= '1';
-		DRAM_ADDR <= "00000000010";
-		wait for clock_period;
-		
-		FIFO1_WR_EN <= '0';
-		FIFO2_WR_EN <= '0';
-		Input <= "1111111111111111";
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '1';
-		DRAM_WR_EN <= '1';
-		MUX_SEL <= '0';
-		DRAM_ADDR <= "00000000011";
-		wait for clock_period;
-
-		MUX_SEL <= '1';
-		FIFO1_RD_EN <= '0';
-		FIFO2_RD_EN <= '0';
-		DRAM_ADDR <= "00000000011";
-		
-		
       -- insert stimulus here 
 
       wait;
