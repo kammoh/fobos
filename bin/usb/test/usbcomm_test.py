1b4059accde7243526a7c6d0c4502541dcaaa24e
Rajesh Velegalati <rvelegal@gmu.edu>
2013-10-27 18:01:08 -0700

    added files

--- bin/usb/test/usbcomm_test.py
+++ bin/usb/test/usbcomm_test.py
@@ -75,8 +75,8 @@ if(dataToStream == OPENADC):
   status = putRegByte(USBHandle, 0x30, 0x01, debug)
   status = putRegByte(USBHandle, 0x30, 0x00, debug)
   time.sleep(1)
-  status = putRegByte(USBHandle, 0x61, 0x00, debug)
-  status = putRegByte(USBHandle, 0x60, 0x00, debug)
+  status = putRegByte(USBHandle, 0x61, 0x00, debug) ## Amp Hi ->01 Lo -> 00
+  status = putRegByte(USBHandle, 0x60, 0x00, debug) ## Gain Values - 0-255 
   
 
 startTime = time()
