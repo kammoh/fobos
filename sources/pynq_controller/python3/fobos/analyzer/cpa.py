e8e41d896b0dcab57d01f9bf72819c9f07268dc3
Abubakr Abdulgadir <aabdulg@gmu.edu>
2019-04-19 12:26:28 -0400

    Re-organized CPA attack code and AES hypothetical power generation code.

--- sources/pynq_controller/python3/fobos/analyzer/cpa.py
+++ sources/pynq_controller/python3/fobos/analyzer/cpa.py
@@ -10,6 +10,7 @@ matrix H to obtain the correlation matrix R
 6- Generate plots.
 """
 import numpy as np
+import scipy.stats.stats as statModule
 
 class CPA():
     """
@@ -110,15 +111,62 @@ def AESSboxOutFirstRound(P):
         for i in range(P.shape[0]):
             O[i,k] = sbox[k ^ P[i,0]]
     return O
+#################################################################
 
-def printHexMatrix(A, printAll=False):
+def getSbox(p, k):
+    return sbox[p]
+
+def vectAESSboxOut(P):
+    """
+    calculates one byte of the AES sbox output 
+    parameters:
+    P : a colomn vector of the sbox input.
+    returns:
+    The output of the sbox in round 1
+    """
+    vectFirstRound = np.vectorize(getSbox)
+    numKeyHypotheses = 256
+    #print(P)
+    numPlainTexts = P.shape[0]
+    O = np.empty((numPlainTexts, numKeyHypotheses), dtype=np.uint8)
+    for k in range(numKeyHypotheses):
+        O[:,k] = vectFirstRound(P.reshape(numPlainTexts,), k)
+    return O
+
+##################################################################
+
+def firstRound(p, k):
+    return sbox[p ^ k]
+
+def vectAESSboxOutFirstRound(P):
+    """
+    calculates one byte of the AES sbox output in the first round
+    parameters:
+    P : a colomn vector of the plain text matrix (corresponds to the key byte
+        to be guessed)
+    returns:
+    The output of the sbox in round 1
+    """
+    vectFirstRound = np.vectorize(firstRound)
+    numKeyHypotheses = 256
+    #print(P)
+    numPlainTexts = P.shape[0]
+    O = np.empty((numPlainTexts, numKeyHypotheses), dtype=np.uint8)
+    for k in range(numKeyHypotheses):
+        O[:,k] = vectFirstRound(P.reshape(numPlainTexts,), k)
+    return O
+##################################################################
+def printHexMatrix(A, printAll=False, dtype='int'):
     import sys
     lim = 10
     if (printAll == True or A.shape[0] < 10):
         lim = A.shape[0]
     for i in range(lim):
         for j in range(A.shape[1]):
-            sys.stdout.write("0x" + format(int(A[i,j]), '02x') + "\t")
+            if dtype == 'float':
+                sys.stdout.write(str(A[i,j]) + "\t")
+            else:
+                sys.stdout.write("0x" + format(int(A[i,j]), '02x') + "\t")
         sys.stdout.write("\n")
 
 def loadTextMatrix(fileName, numCols = 16):
@@ -128,35 +176,336 @@ def loadTextMatrix(fileName, numCols = 16):
     """
     return np.loadtxt(fileName, dtype='uint8', delimiter=' ',
                  converters={_:lambda s: int(s, 16) for _ in range(numCols)})
+##################################################################
+def testAESPowerModel(P1, P2):
+    #vectFirstRound = np.vectorize(firstRound)
+    numPlainTexts = P1.shape[0]
+    z = np.zeros((1, P1.shape[1]))
+    tmp = np.vstack((z, P1))
+    P1 = tmp[0:numPlainTexts,:]
+    O = np.empty(P2.shape, dtype=np.uint8)
+    for i in range(P2.shape[0]):
+        for j in range(P2.shape[1]):
+            O[i,j] = getByteHW(int(P1[i,j]) ^ int(P2[i,j]))
+    return O
+
+def correlation_pearson(measuredData, modeledData):
+    #measuredData = np.transpose(measuredData)
+    corrMatrix = np.zeros(0)
+    corrArray =  np.zeros(0)
+    corrMatrix = np.empty((modeledData.shape[1], measuredData.shape[1]))
+    for i in range( modeledData.shape[1]):
+        colH = modeledData[:,i]
+        for j in range(measuredData.shape[1]) :
+            colM = measuredData[:,j]
+            c, p = statModule.pearsonr(colH, colM)
+            corrMatrix[i,j] = c
+    return corrMatrix
+
+def vectTestAESPowerModel(P1, P2):
+    HW = np.array([0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 
+
+        2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 
+
+        2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 
+
+        4, 5, 5, 6, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 
+
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 
+
+        3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 
+
+        4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 
+
+        3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 
+
+        2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 
+
+        4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4, 4, 5, 
+
+        3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 
+
+        5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 
+
+        4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8], dtype=np.uint8)
+
+    vectGetByteHW = np.vectorize(getByteHW)
+    numPlainTexts = P1.shape[0]
+    z = np.zeros((1, P1.shape[1]), dtype=np.uint8)
+    tmp = np.vstack((z, P1))
+    P1 = tmp[0:numPlainTexts,:]
+    O = np.empty(P2.shape, dtype=np.uint8)
+    for k in range(P2.shape[1]):
+        x= P1[:,k] ^ P2[:,k]
+        O[:,k] = HW[P1[:,k] ^ P2[:,k]]
+    return O
+####################################################################
+def getByteHW(a):
+    return (a&1) + ((a&2)>>1) + ((a&4)>>2) + ((a&8)>>3) \
+            + ((a&16)>>4) + ((a&32)>>5) + ((a&64)>>6) + ((a&128)>>7)
+
+def getPW(p1,p2):
+    t = p1 ^ p2
+    for i in range(len(t)):
+        a = t[i]
+        tmp[i] = (a&1) + ((a&2)>>1) + ((a&4)>>2) + ((a&8)>>3) \
+                + ((a&16)>>4) + ((a&32)>>5) + ((a&64)>>6) + ((a&128)>>7)
+
+def aesPowerHD(p1, p2):
+    tmp = p1 ^ p2
+
+def testAESPowerModel1():
+    plaintext = loadTextMatrix('./example_data/plaintexts1.txt')
+    #plaintext = loadTextMatrix('./plaintext1.txt', 3)
+    print("plaintext=")
+    printHexMatrix(plaintext[:,0:5])
+    numPlainTexts = plaintext.shape[0]
+
+    ciphertext = loadTextMatrix('./example_data/ciphertexts1.txt')
+    #plaintext = loadTextMatrix('./plaintext1.txt', 3)
+    print("ciphertext=")
+    printHexMatrix(ciphertext)
+    hypotheticalPower = []
+    for byteNum in range(16):
+        sbox_pt_key = vectAESSboxOutFirstRound(plaintext[:, byteNum].reshape(numPlainTexts,1))
+        #res =  firstRound(plaintext[:,0], 0)
+        print("sbox_pt_key=")
+        printHexMatrix(sbox_pt_key[:,0:1])
+        #####
+        sbox_ct = vectAESSboxOut(ciphertext[:, byteNum].reshape(numPlainTexts,1))
+        #res =  firstRound(plaintext[:,0], 0)
+        print("sbox_ct=")
+        printHexMatrix(sbox_ct[:,0:1])
+        #####
+        oneBytePower = vectTestAESPowerModel(sbox_ct, sbox_pt_key)
+        hypotheticalPower.append(oneBytePower)
+        print("hypothetical power =")
+        printHexMatrix(oneBytePower[:,0:1])
+
+    return hypotheticalPower
+
+def AESPowerModelFirstRound():
+    plaintext = loadTextMatrix('./example_data/plaintexts1.txt')
+    #plaintext = loadTextMatrix('./plaintext1.txt', 3)
+    print("plaintext=")
+    printHexMatrix(plaintext[:,0:5])
+    numPlainTexts = plaintext.shape[0]
+    hypotheticalPower = []
+    for byteNum in range(16):
+        sbox_pt_key = vectAESSboxOutFirstRound(plaintext[:, byteNum].reshape(numPlainTexts,1))
+        #res =  firstRound(plaintext[:,0], 0)
+        print("sbox_pt_key=")
+        printHexMatrix(sbox_pt_key[:,0:1])
+        #####
+        
+        #####
+        hypotheticalPower.append(sbox_pt_key)
+        print("hypothetical power =")
+        printHexMatrix(sbox_pt_key[:,0:1])
+
+    return hypotheticalPower
+
+def adjustSampleSize(sampleLength, dataArray):
+        #print "\tAdjusting Sample Size to ->" + str(sampleLength)
+        temp = dataArray.shape
+        newDataArray = dataArray
+        arrLen = temp[0]
+        #print "Array Length --> " + str(arrLen)
+        if (arrLen == sampleLength):
+            
+            return dataArray
+            
+        elif (arrLen > sampleLength):
+            
+            diff = arrLen-sampleLength
+            for count in range(0,diff):
+                newDataArray = np.delete(newDataArray, -1, 0)
+            return newDataArray 
+            
+        elif (arrLen < sampleLength):
+            
+            diff = sampleLength-arrLen
+            for count in range (0,diff):
+                newDataArray = np.append(newDataArray,0)
+            return newDataArray 
+            
+def detectSampleSize(file_name):
+    """
+    This function calcualates the number of sample per trace. This is needed to do any paddig/truncation to make all 
+    traces the same size. We iterate through the first 10 traces and get the max number of traces
+    """
+    print "DEBUG: Module: analysis, Function: detectSampleSize():"
+    print "----Detecting samples per trace"
+    maxNumOfSamples = 0
+    for traceCount in range (0,10):
+        try:
+            trace = np.load(file_name)
+        except:
+            print "----You have less than 10 samples. It looks that the max number of samples is : " + str(maxNumOfSamples)
+            return maxNumOfSamples
+        numOfSamples = len(trace)
+        if maxNumOfSamples < numOfSamples:
+            maxNumOfSamples = numOfSamples
+    print "----It looks that the max number of samples in the first 10 traces is: " + str(maxNumOfSamples)
+    return maxNumOfSamples
+    
+def read_raw_traces(file_name, num_of_traces):
+    print "Loading raw traces from file: " + file_name  
+    print "Please wait ..."
+    measurementFile = open(file_name, 'r')
+    #Get sample size to adjust all traces to the same number of samples. A better solution is to get the data cleaned up upon acquistion.
+    global samples_per_trace
+    samples_per_trace = detectSampleSize(measurementFile)
+    ##create the array
+    raw_traces = np.empty((num_of_traces, samples_per_trace))
+    #samples_per_trace = 4000
+    #need to reset file handle since we used it 
+    measurementFile.seek(0)
+    for traceCount in range (0,num_of_traces):
+        #print "traceCount= " + str(traceCount)
+        tempArrayMeasurement = np.load(measurementFile)
+        tempArrayMeasurement = adjustSampleSize(samples_per_trace, tempArrayMeasurement)
+        raw_traces[traceCount,:] = tempArrayMeasurement
+    print "Loading done."
+    return raw_traces
+def plotCorr3D(C, fileName= None, show='no'):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import axes3d
+    x = np.linspace(0, 100, 100)
+    y = np.linspace(0, 256, 256)
+    X, Y = np.meshgrid(x, y)
+    Z = C
+    ####
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    #ax.contour3D(X, Y, Z, 50, cmap='binary')
+    #ax.plot_wireframe(X, Y, Z, 50, cmap='binary')
+    #ax.contour(X, Y, Z, 50)
+    ax.plot_surface(X,Y,Z, rstride=1, cstride=1,
+                cmap='Reds', edgecolor='none')
 
 
-def main():
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    if fileName != None:
+        fig.savefig(fileName)
+    if show == 'yes':
+        plt.show()
+
+
+def plotCorr(C, correctIndex,fileName= None, show='no'):
+    import matplotlib.pyplot as plt
+    plt.clf()
+    for i in range(C.shape[0]):
+        row = C[i,:]
+        plt.plot(row, '#aaaaaa')
+    plt.plot(C[correctIndex, :], 'k')
+    if fileName != None:
+        plt.savefig(fileName)
+    if show == 'yes':
+        plt.show()
+
+def  findCorrectKey(C):
+    maxCorrs = np.amax(C, 0)
+    maxKeyIndexes = np.argmax(C, 0)
+    maxKeyIndex = maxKeyIndexes[np.argmax(maxCorrs)]
+    maxCorr = np.max(maxCorrs)
+    maxCorrTime = np.argmax(maxCorrs)
+    return maxKeyIndex, maxCorr, maxCorrTime
+
+def plotMTDGraph(correctTime, correctKeyIndex, measuredPower, 
+                hypotheticalPower, numTraces=None, stride=1,
+                fileName= None, show='no'):
+    if numTraces == None:
+        numTraces = measuredPower.shape[0]
+    numKeys = hypotheticalPower.shape[1]
+    dataToPlot = np.empty((numKeys,numTraces/ stride))
+    interestingPower = measuredPower[:,correctTime].reshape(measuredPower.shape[0],1)
+    index = 0
+    for i in range(0,numTraces, stride):
+        if i % 100 == 0:
+            print("MDT step={}".format(i))
+        C = correlation_pearson(interestingPower[0:i,:], hypotheticalPower[0:i,:])
+        #print(C.shape)
+        dataToPlot[:,index] = C.reshape(numKeys)
+        index +=1
+    import matplotlib.pyplot as plt
+    plt.clf()
+    for i in range(dataToPlot.shape[0]):
+        row = dataToPlot[i,:]
+        plt.plot(row, '#aaaaaa')
+    plt.plot(dataToPlot[correctKeyIndex, :], 'k')
+    if fileName != None:
+        plt.savefig(fileName)
+    if show == 'yes':
+        plt.show()
+
+def testModel1():
     ## Test genIntermediateMatrix
     D = np.random.randint(0, 10, size=(10,1))
     K = np.random.randint(0, 10, size=(1 ,5))
     cpa = CPA()
-    #V = cpa.genIntermediateMatrix(D, K, exampleMappingFunction1)
-    #print("D=")
-    #printHexMatrix(D)
-    #print("K=")
-    #printHexMatrix(K)
-    #print("V=")
-    #printHexMatrix(V)
 
-    #A = loadTextMatrix('./example_data/ciphertexts1.txt')
-    #print("A=")
-    #printHexMatrix(A)
-    ####test aes sbox fiirst round
-    #plaintext = loadTextMatrix('./example_data/ciphertexts1.txt')
-    plaintext = loadTextMatrix('./plaintext1.txt', 3)
-    print("plaintext=")
-    printHexMatrix(plaintext)
-    keys = loadTextMatrix('./example_data/keys1.txt')
-    print (plaintext)
-    numPlainTexts = plaintext.shape[0]
-    res = AESSboxOutFirstRound(plaintext[:, 0].reshape(numPlainTexts,1))
-    print("RES=")
-    printHexMatrix(res)
+    hypotheticalPower = testAESPowerModel1()
+    measuredPower = read_raw_traces("./example_data/rawDataAligned.npy", 10000)
+    print(measuredPower.shape)
+    #print(hypotheticalPower.shape)
+    croppedMeasuredPower = measuredPower[:,100:400]
+    correctKey = []
+    for byteNum in range(16):
+        C =  correlation_pearson(croppedMeasuredPower, hypotheticalPower[byteNum][0:10000,:])
+        print("C=")
+        print(C.shape)
+        printHexMatrix(C, dtype='float')
+        maxKeyIndex, maxCorr, maxCorrTime = findCorrectKey(C)
+        corrFile = './analysis/correlation' + str(byteNum)
+        mtdFile = './analysis/MTD' + str(byteNum)
+        print("keyIndex= {}, max corr = {}, time= {}".format(hex(maxKeyIndex), maxCorr, maxCorrTime))
+        plotCorr(C, maxKeyIndex, fileName= corrFile)
+        plotMTDGraph(maxCorrTime, maxKeyIndex, croppedMeasuredPower, hypotheticalPower[byteNum], 
+            stride=100, fileName=mtdFile, show='no')
+        correctKey.append(format(maxKeyIndex, '02x'))
+
+    print(correctKey)
+
+def testFirstRound():
+    ## Test genIntermediateMatrix
+    D = np.random.randint(0, 10, size=(10,1))
+    K = np.random.randint(0, 10, size=(1 ,5))
+    cpa = CPA()
+
+    hypotheticalPower = AESPowerModelFirstRound()
+    measuredPower = read_raw_traces("./example_data/rawDataAligned.npy", 10000)
+    print(measuredPower.shape)
+    #print(hypotheticalPower.shape)
+    croppedMeasuredPower = measuredPower[:,100:400]
+    correctKey = []
+    correctSample = []
+    for byteNum in range(16):
+        C =  correlation_pearson(croppedMeasuredPower, hypotheticalPower[byteNum][0:10000,:])
+        print("C=")
+        print(C.shape)
+        printHexMatrix(C, dtype='float')
+        maxKeyIndex, maxCorr, maxCorrTime = findCorrectKey(C)
+        corrFile = './analysis/correlation' + str(byteNum)
+        mtdFile = './analysis/MTD' + str(byteNum)
+        print("keyIndex= {}, max corr = {}, time= {}".format(hex(maxKeyIndex), maxCorr, maxCorrTime))
+        plotCorr(C, maxKeyIndex, fileName= corrFile)
+        plotMTDGraph(maxCorrTime, maxKeyIndex, croppedMeasuredPower, hypotheticalPower[byteNum], 
+            stride=100, fileName=mtdFile, show='no')
+        correctKey.append(format(maxKeyIndex, '02x'))
+        correctSample.append(format(maxCorrTime, '02x'))
+
+    print(correctKey)
+    print(correctSample)
+
+def main():
+    #testFirstRound()
+    testModel1()
+
+
+    
 
 if __name__ == '__main__':
     main()
