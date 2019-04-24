ea91b95d4f7fe347b3d190df85c2f346c3135c58
Abubakr Abdulgadir <aabdulg@gmu.edu>
2019-04-24 17:50:25 -0400

    Re-organized online t-test code

--- sources/pynq_controller/python3/fobos/analyzer/ttestOnline.py
+++ sources/pynq_controller/python3/fobos/analyzer/ttestOnline.py
@@ -12,58 +12,76 @@ end_ylim = 40
 start_xlim = 0
 end_xlim = 1000
 
-k0 = None
-M0 = None
-S0 = None
-k1 = None
-M1 = None
-S1 = None
+class TtestOnline():
+   
+   def __init__(self, numSamples, threshold = 4.5):
+      self.k0 = np.zeros((1, numSamples))
+      self.M0 = np.zeros((1, numSamples))
+      self.S0 = np.zeros((1, numSamples))
+      self.k1 = np.zeros((1, numSamples))
+      self.M1 = np.zeros((1, numSamples))
+      self.S1 = np.zeros((1, numSamples))
+      self.threshold = threshold
+      self.numSamples = numSamples
 
-def update(x0, x1):
-   global k0
-   global M0
-   global S0
-   k0 += 1
-   newM0 = M0 + (x0 - M0) * 1./ k0
-   newS0 = S0 + (x0 - M0) * (x0 - newM0)
-   M0, S0 = newM0, newS0
-   global k1
-   global M1
-   global S1
-   k1 += 1
-   newM1 = M1 + (x1 - M1) * 1./ k1
-   newS1 = S1 + (x1 - M1) * (x1 - newM1)
-   M1, S1 = newM1, newS1
+   def addTrace(self, x, setNum):
+      #global k0
+      #global M0
+      #global S0
+      if setNum == 0: 
+         self.k0 += 1
+         newM0 = self.M0 + (x - self.M0) * 1./ self.k0
+         newS0 = self.S0 + (x - self.M0) * (x - newM0)
+         self.M0, self.S0 = newM0, newS0
+      else:
+         self.k1 += 1
+         newM1 = self.M1 + (x - self.M1) * 1./ self.k1
+         newS1 = self.S1 + (x - self.M1) * (x - newM1)
+         self.M1, self.S1 = newM1, newS1
 
-   #print(M0)
-   #print(M1)
+   def calcTvalues(self):
+      var0 = self.S0 / (self.k0 - 1)
+      var1 = self.S1 / (self.k1 - 1)
+      t_array = (self.M0 - self.M1)/ np.sqrt(var0/self.k0 + var1/self.k1)
+      #print t_array.shape
+      #print t_array
+      ##check if test passed
+      passed = (t_array > self.threshold).sum() == 0
+      return t_array.reshape(t_array.shape[1]), passed
+
+   def plot(self, fileName):
+      plt.clf()
+      t_array, passed = self.calcTvalues()
+      threshold = [self.threshold] * self.numSamples
+      minus_threshold = [-1 * self.threshold] * self.numSamples
+      plt.plot(threshold, color='b')
+      plt.plot(minus_threshold, color='b')
+      plt.ylim(start_ylim, end_ylim)
+      plt.xlim(start_xlim, end_xlim)
+      plt.xlabel("Sample No.")
+      plt.ylabel("t-value")
+      plt.plot(t_array, color='r')
+      plt.savefig(fileName)
+
 
 def ttestOnline(traces0, traces1, numTraces):
    numSamples = traces0.shape[1]
-   from scipy import stats
    traces0 = traces0[0:numTraces,:]
    traces1 = traces1[0:numTraces,:]
+   ttest = TtestOnline(numSamples)
    for i in range(numTraces):
       x0 = traces0[i, :]
       x1 = traces1[i, :]
-      update(x0, x1)
-   var0 = S0 / (k0 - 1)
-   var1 = S1 / (k1 - 1)
-   n = traces0.shape[0]
-   t_array = (M0 - M1)/ np.sqrt(var0/n + var1/n)
-   print t_array.shape
-   print t_array
-   return t_array.reshape(numSamples)
+      ttest.addTrace(x0, 0)
+      ttest.addTrace(x1, 1)
+      if i  % 500 == 0 and i > 1:
+         print(i)
+         #print(ttest.k0, ttest.k1)\
+         ttest.plot("fig-{}.png".format(i))
+   t_array, passed = ttest.calcTvalues()
+   print("T-test passed= {}".format(passed))
+   return t_array
 
-def ttest(traces0, traces1, numTraces):
-   from scipy import stats
-   traces0 = traces0[0:numTraces,:]
-   traces1 = traces1[0:numTraces,:]
-   numSamples = traces0.shape[1]
-   t = np.empty((numSamples))
-   for i in range(numSamples):
-      t[i] = stats.ttest_ind(traces0[:,i], traces1[:,i])[0]
-   return t
 
 def main():
 
@@ -82,19 +100,7 @@ def main():
    #num_of_traces = raw_traces0.shape[0]
    samples_per_trace = raw_traces0.shape[1]
    print "samples_per_trace=", samples_per_trace
-   global k0
-   global M0
-   global S0
-   global k1
-   global M1
-   global S1
-   k0 = np.zeros((1, samples_per_trace))
-   M0 = np.zeros((1, samples_per_trace))
-   S0 = np.zeros((1, samples_per_trace))
    
-   k1 = np.zeros((1, samples_per_trace))
-   M1 = np.zeros((1, samples_per_trace))
-   S1 = np.zeros((1, samples_per_trace))
    
    t_array = ttestOnline(raw_traces0, raw_traces1, 4000)
    threshold = [4.5] * samples_per_trace
